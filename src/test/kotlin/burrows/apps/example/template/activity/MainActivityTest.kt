f44df4473ebca270527104bcd9f0b1cba889b09b
Jared Burrows <jaredsburrows@gmail.com>
2016-07-16 18:34:24 -0700

    updates

--- src/test/kotlin/burrows/apps/example/template/activity/MainActivityTest.kt
+++ src/test/kotlin/burrows/apps/example/template/activity/MainActivityTest.kt
@@ -1,16 +1,20 @@
-package burrows.apps.example.template.activity;
+package burrows.apps.example.template.activity
 
+import android.os.Build
+import burrows.apps.example.template.BuildConfig
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
-import org.robolectric.RobolectricGradleTestRunner
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 /**
  * @author <a href="mailto:jaredsburrows@gmail.com">Jared Burrows</a>
  * @since 0.0.1
  */
-@RunWith(RobolectricGradleTestRunner::class)
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = intArrayOf(Build.VERSION_CODES.M))
 class MainActivityTest {
 
     @Test
